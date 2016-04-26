1c321f177152c4b77524fa3e39580833398a3192
ferrone <tgrousset@orange.fr>
2016-04-26 11:49:50 +0200

    nico

--- Matrice.h
+++ Matrice.h
@@ -1,35 +1,47 @@
 #include <iostream>
 #include <vector>
 
+struct Case{
+	int 	caseLigne;
+	int 	caseColonne;
+	double 	caseValeur;
+};
+
 class Matrice{
 
 	private:
-
-	std::vector<std::vector<double>> matrice;					//plus facile à utiliser que double**
 	int tailleLigne;
 	int tailleColonne;
+	int nombreCases = 0;
+	std::vector<Case> cases;
 
 	public:
-
 	Matrice(int ligne = 10000, int colonne = 10000);			//création d'une matrice de taille int,int
-	void NewMatrice(int,int);
+	Matrice(std::string);
 	~Matrice();													//grace au vecteur n'a rien à détruire
-	void Initialisation();										//le gars va créer sa matrice. (while[choix == "oui"] -> rentre sa case avec Modification).
-	void Charger(std::string);									//matrice venant d'un fichier.txt
-	void Sauvegarder(std::string);								//sauvegarde la matrice en LVC dans un fichier.txt
-	void Modification(int,int,double);							//permet de modifier une case en particulier. (sera appelée dans initialisation)
-	void Transposer();											//transpose la matrice
-	Matrice& operator+=(const Matrice&);
-	Matrice& operator-=(const Matrice&);
-	Matrice operator=(const Matrice&);
 	int TailleLigne() const;									//getter du nombre de ligne
 	int TailleColonne() const;									//getter du nombre de colonne
-	double& operator()(int ligne, int colonne);					//getter du vecteur. (retourne la case i,j de la matrice).
-	double operator()(int ligne, int colonne) const;
+	int NombreCases() const;
+	Case& operator()(int element);								//getter du vecteur. (retourne la case i,j de la matrice).
+	Case operator()(int element) const;
+	void Initialisation();										//le gars va créer sa matrice. (while[choix == "oui"] -> rentre sa case avec AjouterCase).
+	void Charger(std::string);									//matrice venant d'un fichier.txt
+	void Sauvegarder(std::string);								//sauvegarde la matrice en LVC dans un fichier.txt
+	bool AjouterCase(int,int,double);							//permet de modifier une case en particulier. (sera appelée dans initialisation)
+	void ModifierCase(int,int,double);
+	bool EstPresent(Case element);
+	void SortCases();
+	void Transposer();											//transpose la matrice
+	Matrice operator=(const Matrice&);
+	Matrice& operator+=(const Matrice&);
+	Matrice& operator-=(const Matrice&);
 };
 
-bool OperationPossible(const Matrice&, const Matrice&);			//M1(a,b) * M2(b,c) -> M3(a,c)  sinon return false
-Matrice operator+(const Matrice&,const Matrice&);			//Dans chaque fonction faudra créer une matrice tampon pour pas modifier celles d'origine
+Matrice operator+(const Matrice&,const Matrice&);
 Matrice operator*(const Matrice&,const Matrice&);
 Matrice operator-(const Matrice&,const Matrice&);
 std::ostream& operator<<(std::ostream& flux, const Matrice&);	//affiche une matrice. (seulement les cases pleines).
+int CheckAllChar(int i, std::string str, int taille);
+int CheckStringToInt(std::string mot);
+double CheckStringToDouble(std::string mot);
+bool Init(int lig, int col, int& i, int& j, double& valeur);
