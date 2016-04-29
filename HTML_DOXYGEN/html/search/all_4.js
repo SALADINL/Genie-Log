53571883c3ff234935d4a8babbbbd6becfc07e43
shodream <leo_84@hotmail.fr>
2016-04-29 09:10:28 +0200

    modif doc nico

--- HTML_DOXYGEN/html/search/all_4.js
+++ HTML_DOXYGEN/html/search/all_4.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['init',['Init',['../Matrice_8cpp.html#aac431b5a5d4070d1558a297f831b4791',1,'Matrice.cpp']]],
   ['initialisation',['Initialisation',['../classMatrice.html#af2e5ed60f4d4ccbbc16140fa4e4d0ef0',1,'Matrice']]]
 ];
